2f16fee57b98e851e96276fd92a8a46a918c2476
brightonmboya <b.mboya@alustudent.com>
2024-08-02 13:58:03 +0300

    added change team member role

---
+++ src/server/api/routers/workspaces/change-team-member-role.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "../../trpc";
+import { roles } from "~/utils/types";
+
+export const changeTeamMemberRole = createTRPCRouter({
+  changeRole: protectedProcedure
+    .input(
+      z.object({
+        role: z.enum(roles),
+        userId: z.string(),
+        workspaceSlug: z.string(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const workspace = await ctx.db.project.findUnique({
+        where: {
+          slug: input.workspaceSlug,
+        },
+        select: {
+          id: true,
+        },
+      });
+      return await ctx.db.projectUsers.update({
+        data: {
+          role: input.role,
+        },
+        where: {
+          userId_projectId: {
+            userId: input.userId,
+            projectId: workspace?.id!,
+          },
+        },
+      });
+    }),
+});
