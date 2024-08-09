95c7ff6235cc167fd872408a2deb3a400d8e5f5c
brightonmboya <b.mboya@alustudent.com>
2024-08-09 17:06:15 +0300

    rendered all blog posts

---
+++ src/app/(marketing)/blogs/_components/blog-card.tsx
@@ -0,0 +1,88 @@
+import type { Author } from "~/content/blog/authors";
+import { cn } from "~/utils";
+import { format } from "date-fns";
+import { Frame } from "./frame";
+import { ImageWithBlur } from "./ImageWithBlur";
+import { ShadCnAvatar as Avatar, AvatarFallback, AvatarImage } from "~/components/ui";
+
+type BlogCardProps = {
+  tags?: string[];
+  imageUrl?: string;
+  title?: string;
+  subTitle?: string;
+  author: Author;
+  publishDate?: string;
+  className?: string;
+};
+
+export function BlogCard({
+  tags,
+  imageUrl,
+  title,
+  subTitle,
+  author,
+  publishDate,
+  className,
+}: BlogCardProps) {
+  return (
+    <div
+      className={cn(
+        "flex flex-col rounded-3xl border border-transparent p-3 duration-150 ease-out hover:border-neutral-900 hover:bg-neutral-950 max-sm:h-full",
+        className,
+      )}
+    >
+      <div className="w-full rounded-2xl bg-clip-border">
+        <Frame size="sm">
+          <div className="relative aspect-video">
+            <ImageWithBlur
+              placeholder="blur"
+              blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8+e1bKQAJMQNc5W2CQwAAAABJRU5ErkJggg=="
+              src={imageUrl!}
+              alt="Hero Image"
+              className="w-full overflow-hidden object-center"
+              fill={true}
+            />
+          </div>
+        </Frame>
+      </div>
+      <div className="flex h-full flex-col px-1">
+        <div className="flex h-80 flex-col pb-2 pt-6">
+          <div className="flex-inline flex h-6 flex-wrap gap-4">
+            {tags?.map((tag) => (
+              <div className="content-center rounded-md bg-white/10 px-[9px] text-sm text-white/50">
+                {tag.charAt(0).toUpperCase() + tag.slice(1)}
+              </div>
+            ))}
+          </div>
+          <h2 className="blog-heading-gradient mt-6 flex justify-start text-xl font-medium leading-10 sm:text-2xl md:text-2xl">
+            {title}
+          </h2>
+
+          <p className="mt-6 h-full text-base font-normal leading-6 text-white/60 sm:text-sm">
+            {subTitle}
+          </p>
+          {/* Todo: Needs ability to add multiple authors at some point */}
+          <div className="flex h-full flex-col flex-wrap justify-end">
+            <div className="flex flex-row">
+              <Avatar className="h-8 w-8">
+                <AvatarImage
+                  alt={author.name}
+                  src={author.image.src}
+                  width={12}
+                  height={12}
+                />
+                <AvatarFallback />
+              </Avatar>
+              <p className="ml-4 pt-3 text-sm font-medium text-white">
+                {author.name}
+              </p>
+              <p className="ml-6 pt-3 text-sm font-normal text-white/50">
+                {format(new Date(publishDate!), "MMM dd, yyyy")}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/(marketing)/blogs/page.tsx
@@ -0,0 +1,72 @@
+import { BlogHero } from "./_components/blog-hero";
+import { BlogGrid } from "./_components/blog-grid";
+// import { CTA } from "@/components/cta";
+// import {
+//   TopLeftShiningLight,
+//   TopRightShiningLight,
+// } from "@/components/svg/background-shiny";
+// import { MeteorLinesAngular } from "@/components/ui/meteorLines";
+import { authors } from "~/content/blog/authors";
+import { type Post, allPosts } from "content-collections";
+import Link from "next/link";
+
+export const metadata = {
+  title: "Blog | Jani AI",
+  description: "Latest blog posts and news from Jani AI team.",
+  openGraph: {
+    title: "Blog | Jani AI",
+    description: "Latest blog posts and news from Jani AI team.",
+    url: "https://jani-ai.com/blogs",
+    siteName: "unkey.com",
+    images: [
+      {
+        url: "",
+        width: 1200,
+        height: 675,
+      },
+    ],
+  },
+  twitter: {
+    title: "Blog | Jani AI",
+    card: "summary_large_image",
+  },
+};
+
+type Props = {
+  searchParams?: {
+    tag?: string;
+    page?: number;
+  };
+};
+
+export default async function Blog(props: Props) {
+
+  const posts = allPosts.sort((a: Post, b: Post) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
+  });
+  const blogGridPosts = posts.slice(1, posts.length);
+
+  return (
+    <>
+      <div className="container mx-auto w-full overflow-hidden scroll-smooth pt-48">
+     
+        <div className="mx-0 w-full rounded-3xl px-0">
+          <Link href={`${posts[0]?.url}`} key={posts[0]?.url}>
+            <BlogHero
+              tags={posts[0]?.tags}
+              imageUrl={
+                posts[0]?.image ?? "/images/blog-images/defaultBlog.png"
+              }
+              title={posts[0]?.title}
+              subTitle={posts[0]?.description}
+              author={authors[posts[0]?.author!]!}
+              publishDate={posts[0]?.date}
+            />
+          </Link>
+        </div>
+        <BlogGrid posts={blogGridPosts} searchParams={props.searchParams} />
+
+      </div>
+    </>
+  );
+}
