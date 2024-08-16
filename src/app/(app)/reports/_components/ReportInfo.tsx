c08752a7534e12c07d14011f5282a83f8200923b
brightonmboya <b.mboya@alustudent.com>
2024-08-16 22:21:03 +0300

    fixed the report misdirection error

---
+++ src/app/(app)/reports/_components/ReportInfo.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React from "react";
+import { EmptyState } from "~/components/shared/empty/empty-state";
+import LoadingSkeleton from "~/components/ui/LoadingSkeleton";
+import { api } from "~/trpc/react";
+import { useSearchParams } from "next/navigation";
+import PDFReport from "../../dashboard/[accountSlug]/reports/_components/PDFReport";
+import { useToast } from "~/utils/hooks";
+
+export default function ReportViewPage() {
+  const searchParams = useSearchParams();
+  const reportId = searchParams.get("reportId");
+  const workspaceSlug = searchParams.get("workspaceSlug");
+
+  const { toast } = useToast();
+  const { data, isLoading, isError } = api.reports.fetchById.useQuery({
+    reportId: reportId as unknown as string,
+    workspaceSlug: workspaceSlug as unknown as string,
+  });
+  isError &&
+    toast({
+      description: "Failed to load the report",
+      variant: "destructive",
+    });
+
+  return (
+    <main>
+      {isLoading && <LoadingSkeleton />}
+      {!isLoading && !data && (
+        <EmptyState
+          // @ts-ignore
+          customContent={{
+            title: "No Reports found",
+            text: "The requested report was not found.",
+          }}
+        />
+      )}
+
+      {!isLoading && data && <PDFReport data={data} />}
+    </main>
+  );
+}
