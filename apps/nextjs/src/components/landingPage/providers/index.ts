60b7ac9f802cc896f0b5e4f46cdf894e6e86c546
brightonmboya <b.mboya@alustudent.com>
2026-03-04 17:16:28 +0200

    remove the page loader components

--- apps/nextjs/src/components/landingPage/providers/index.ts
+++ apps/nextjs/src/components/landingPage/providers/index.ts
@@ -1,3 +1,1 @@
-export { default as PageLoader } from './PageLoader'
-export { default as SmoothScrollProvider } from './SmoothScrollProvider'
 export { default as InfiniteScroll } from './InfiniteScroll'
